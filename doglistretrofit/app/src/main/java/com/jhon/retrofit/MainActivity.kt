0aee2eaeff2c08acc46c8e2cec925077d31e532b
jquinterom <jfredyquintero@hotmail.com>
2022-03-17 11:33:37 -0500

    Retrofit project

---
+++ doglistretrofit/app/src/main/java/com/jhon/retrofit/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.jhon.retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.jhon.retrofit.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: DogAdapter
+    private val dogImages = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.svDogs.setOnQueryTextListener(this)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        adapter = DogAdapter(dogImages)
+        binding.rvDogs.layoutManager = LinearLayoutManager(this)
+        binding.rvDogs.adapter = adapter
+    }
+
+    // Instancia de objeto retrofit
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    // Llamadas asincronas
+    private fun searchByName(query: String) {
+        CoroutineScope(
+            Dispatchers.IO
+        ).launch {
+            // Todo esto es en segundo plano
+            val call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val puppies = call.body()
+            runOnUiThread{
+                if(call.isSuccessful){
+                    // Mostrar las imagenes en el recyclerview
+                    val images = puppies?.images ?: emptyList()
+                    dogImages.clear()
+                    dogImages.addAll(images)
+                    adapter.notifyDataSetChanged()
+                } else {
+                    // Mostrar error
+                    showError()
+                }
+                hideKeyboard()
+            }
+        }
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.viewRoot.windowToken, 0)
+    }
+
+    private fun showError() {
+        Snackbar.make(binding.root, "Ha ocurrido un error", Snackbar.LENGTH_LONG).show()
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if(!query.isNullOrEmpty()){
+            searchByName(query.lowercase())
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
+}
