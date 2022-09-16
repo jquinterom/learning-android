716906744d10947502cec653a7549ec2d0ab97b4
jquinterom <jfredyquintero@hotmail.com>
2022-09-16 17:36:39 -0500

    Creando repositorio para el clasificador

--- dogedex/app/src/main/java/com/jhon/dogedex/main/MainViewModel.kt
+++ dogedex/app/src/main/java/com/jhon/dogedex/main/MainViewModel.kt
@@ -1,13 +1,18 @@
 package com.jhon.dogedex.main
 
+import androidx.camera.core.ImageProxy
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jhon.dogedex.api.ApiResponseStatus
 import com.jhon.dogedex.doglist.DogRepository
+import com.jhon.dogedex.machinelearning.Classifier
+import com.jhon.dogedex.machinelearning.ClassifierRepository
+import com.jhon.dogedex.machinelearning.DogRecognition
 import com.jhon.dogedex.model.Dog
 import kotlinx.coroutines.launch
+import java.nio.MappedByteBuffer
 
 class MainViewModel : ViewModel() {
 
@@ -19,7 +24,24 @@ class MainViewModel : ViewModel() {
     val status: LiveData<ApiResponseStatus<Dog>>
         get() = _status
 
+    private val _dogRecognition = MutableLiveData<DogRecognition>()
+    val dogRecognition: LiveData<DogRecognition>
+        get() = _dogRecognition
+
     private val dogRepository = DogRepository()
+    private lateinit var classifierRepository: ClassifierRepository
+
+    fun setupClassifier(tfLiteModel: MappedByteBuffer, labels: List<String>) {
+        val classifier = Classifier(tfLiteModel, labels)
+        classifierRepository = ClassifierRepository(classifier)
+    }
+
+    fun recognizeImage(imageProxy: ImageProxy) {
+        viewModelScope.launch {
+            _dogRecognition.value = classifierRepository.recognizeImage(imageProxy)
+            imageProxy.close()
+        }
+    }
 
     fun getDogByMlId(mlDogId: String) {
         viewModelScope.launch {
