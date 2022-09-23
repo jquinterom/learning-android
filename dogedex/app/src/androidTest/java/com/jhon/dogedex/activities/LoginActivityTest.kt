034a3eda9c1b18476a9886e3d0fe0487ed4feda7
jquinterom <jfredyquintero@hotmail.com>
2022-09-23 08:44:53 -0500

    Cambiando el modulo para testing

--- dogedex/app/src/androidTest/java/com/jhon/dogedex/activities/LoginActivityTest.kt
+++ dogedex/app/src/androidTest/java/com/jhon/dogedex/activities/LoginActivityTest.kt
@@ -1,12 +1,22 @@
 package com.jhon.dogedex.activities
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.jhon.dogedex.auth.AuthRepository
 import com.jhon.dogedex.auth.LoginActivity
+import com.jhon.dogedex.di.AuthTasksModule
+import com.jhon.dogedex.interfaces.AuthTasks
+import com.jhon.dogedex.repositories.AuthRepositories
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import dagger.hilt.components.SingletonComponent
 import org.junit.Rule
 
 @HiltAndroidTest
+@UninstallModules(AuthTasksModule::class)
 class LoginActivityTest {
 
     @get:Rule(order = 0)
@@ -15,5 +25,14 @@ class LoginActivityTest {
     @get: Rule(order = 1)
     val composeTestRule = createAndroidComposeRule<LoginActivity>()
 
+    @Module
+    @InstallIn(SingletonComponent::class)
+    abstract class AuthTasksTestModule {
+
+        @Binds
+        abstract fun bindDogTasks(
+            dogRepository: AuthRepositories.FakeAuthRepositoryToLogin
+        ): AuthTasks
+    }
 
 }
--- dogedex/app/src/androidTest/java/com/jhon/dogedex/repositories/AuthRepositories.kt
+++ dogedex/app/src/androidTest/java/com/jhon/dogedex/repositories/AuthRepositories.kt
@@ -3,10 +3,26 @@ package com.jhon.dogedex.repositories
 import com.jhon.dogedex.api.ApiResponseStatus
 import com.jhon.dogedex.interfaces.AuthTasks
 import com.jhon.dogedex.model.User
+import javax.inject.Inject
 
 class AuthRepositories {
 
     class FakeAuthRepository : AuthTasks {
+        override suspend fun login(email: String, password: String): ApiResponseStatus<User> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun signUp(
+            email: String,
+            password: String,
+            passwordConfirmation: String
+        ): ApiResponseStatus<User> {
+            TODO("Not yet implemented")
+        }
+    }
+
+
+    class FakeAuthRepositoryToLogin @Inject constructor() : AuthTasks {
         override suspend fun login(email: String, password: String): ApiResponseStatus<User> {
             TODO("Not yet implemented")
         }
